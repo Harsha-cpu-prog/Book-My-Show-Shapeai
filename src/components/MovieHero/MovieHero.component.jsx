05b9239992081598220dee456dcf72f41a002a31
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-09-18 23:41:21 +0530

    added poster

--- src/components/MovieHero/MovieHero.component.jsx
+++ src/components/MovieHero/MovieHero.component.jsx
@@ -1,30 +1,72 @@
 import React from 'react';
+import MovieInfo from './MovieInfo.component';
 
 const MovieHero = () => {
     return (
         <>
         <div>
-        <div className="md:hidden w-full " style={ { height : "calc(100vw)"}}>
+          {/* mobile */}
+        <div 
+        className=" relative md:hidden w-full"
+         style={{height : "calc(180vw)"}}
+         >
+           <div className="absolute z-20 bottom-4 left-4">
+             <MovieInfo/> 
+           </div>
+          <div className="w-full h-56 bg-opacity-90 absolute bg-black z-10 bottom-0"/>
   <img 
   src= "https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg"
   alt="poster"
   classname="w-full h-full"
   />
-        </div>
-         <div  className=" hidden md:block w-full lg:hidden"
+      </div>
+
+        {/* medium */}
+         <div 
+          className="  hidden md:block w-full lg:hidden"
          style={{height : "calc(100vw)"}}
          >
+           <div className="w-full h-56 bg-opacity-50 absolute bg-black z-10 bottom-0"/>
+           <div className="absolute z-20 bottom-4 ">
+             <MovieInfo/> 
+           </div>
          <img 
   src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg"
  alt="poster"
   classname="w-full h-full"
+/>
+      </div>
 
-  />
-         </div>
-       <div className=" hidden w-full lg:block" style={{height:"30rem"}}>
-     <img  src="https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg"
+         {/* large */}
+       <div className=" relative hidden w-full lg:block"
+        style= {{ height:"30 rem" }}
+        >
+       
+
+       
+         <div className="absolute z-10 w-full h-full"
+          style={{ 
+            backgroundImage:
+            "linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"
+            ,}}
+          />
+
+          <div className="absolute z-30 left-24 top-10 flex items-center gap-10">
+        <div className=" w-64 h-96 ">
+          <img 
+          src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg"
+          alt="poster "
+          className="w-full h-full rounded-xl"
+          />
+        </div>
+        <div><MovieInfo/></div>
+        </div>
+
+     <img 
+      src="https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg"
  alt="poster"
-  classname="w-full h-full"/>
+  classname="w-full h-full "
+  />
             </div>
        </div>
         </>
@@ -32,31 +74,3 @@ const MovieHero = () => {
 }
 
 export default MovieHero;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* 
-<div class="styles__BannerWrapper-sc-qswwm9-1 ketaHv"
- style="background-image: linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 
- 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%),
-  url(&quot;https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg&quot;);"><div class="styles__BannerContainer-sc-qswwm9-2 etlsyE"><div class="styles__MultimediaContainer-sc-qswwm9-4 iquHrU"><section class="styles__MultimediaContainer-sc-bsek5f-0 dcMQvZ"><div type="vertical" class="styles__PosterContainer-sc-echj48-0 fsHyEG"><div class="styles__ImageWrapper-sc-echj48-6 aWjJo" style="border-radius: 16px; object-fit: cover;"><img src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg" alt="Zack Snyder`s Justice League" width="100%" height="100%" class="styles__Image-sc-echj48-5 bRHBJS" style="border-radius: 16px; object-fit: cover;"></div><div class="styles__SVGFrame-sc-xta4k3-0 bxWgHp"><svg width="13" height="13"><use xlink:href="/chunks/icons/synopsis-icons-96c193d5.svg#icon-play-no-bg"></use></svg><span>Trailers (3)</span></div></div></section></div><div class="styles__EventInformationContainer-sc-qswwm9-5 kqaQef"><section class="styles__TagWrapper-sc-qswwm9-10 dcEURT"><img src="https://in.bmscdn.com/moviemode/tvod/premiere-tag.png" alt="tvod-tag" width="90" height="20" class="styles__ImageTag-sc-qswwm9-12 iMjFDD"><div color="#FFFFFF" class="styles__Tag-sc-qswwm9-11 gWxJhw"><span>Streaming Now</span></div></section><h1 class="styles__EventHeading-sc-qswwm9-6 dEOMtn">Zack Snyder`s Justice League</h1><div class="styles__EventAttributesWrapper-sc-2k6tnd-0 faNygP"><div class="styles__EventAttributesContainer-sc-2k6tnd-1 hSMSQi"><div class="styles__LinkedElementsContainer-sc-2k6tnd-4 caXqWn"><a href="https://in.bookmyshow.com/explore/stream-movies-english" class="styles__LinkedElements-sc-2k6tnd-2 gqLZem">English</a></div><span class="styles__Separator-sc-2k6tnd-6 gHHAVj">•</span><div class="styles__LinkedElementsContainer-sc-2k6tnd-4 caXqWn"><a class="styles__LinkedElements-sc-2k6tnd-2 gVVld">Languages:</a><span class="styles__LanguageOptionsElement-sc-2k6tnd-3 gIAGFs">Audio(1), Subtitles(1)</span></div></div><div class="styles__EventAttributesContainer-sc-2k6tnd-1 hSMSQi">4h 1m<span class="styles__Splitter-sc-2k6tnd-5 emtcqy">  •  </span><a href="https://in.bookmyshow.com/explore/stream-movies-action" class="styles__LinkedElements-sc-2k6tnd-2 dvbhHH">Action</a><span class="styles__Splitter-sc-2k6tnd-5 eoFbrH">,</span><a href="https://in.bookmyshow.com/explore/stream-movies-adventure" class="styles__LinkedElements-sc-2k6tnd-2 dvbhHH">Adventure</a><span class="styles__Splitter-sc-2k6tnd-5 eoFbrH">,</span><a href="https://in.bookmyshow.com/explore/stream-movies-fantasy" class="styles__LinkedElements-sc-2k6tnd-2 dvbhHH">Fantasy</a><span class="styles__Splitter-sc-2k6tnd-5 emtcqy">  •  </span>16+<span class="styles__Splitter-sc-2k6tnd-5 emtcqy">  •  </span>18 Mar, 2021</div></div><div class="styles__CtaWrapper-sc-qswwm9-8 JInhj"><div id="page-cta-container" class="styles__PageCtaContainer-sc-11ze7wi-0 dGBfaX"><button data-phase="tvod" class="CommonContainers__ButtonComponent-sc-8f9mtj-0 CommonContainers__CtaComponent-sc-8f9mtj-1 styles__CtaButtonContainer-sc-1vmod7e-0 kVodz"><div class="styles__CtaText-sc-1vmod7e-2 cYhRUm"><div><span style="font-weight: 500; font-size: 16px; color: rgb(255, 255, 255); ">Rent </span><span style="font-weight: 500; font-size: 16px; color: rgb(255, 255, 255); ">₹149</span></div></div></button><button data-phase="tvod" class="CommonContainers__ButtonComponent-sc-8f9mtj-0 CommonContainers__CtaComponent-sc-8f9mtj-1 styles__CtaButtonContainer-sc-1vmod7e-0 kVodz"><div class="styles__CtaText-sc-1vmod7e-2 cYhRUm"><div><span style="font-weight: 500; font-size: 16px; color: rgb(255, 255, 255); ">Buy </span><span style="font-weight: 500; font-size: 16px; color: rgb(255, 255, 255); ">₹689</span></div></div></button></div></div></div><div class="styles__ShareContainer-sc-qswwm9-9 bmUsGK"><section class="styles__ShareWrapper-sc-1vaplep-0 ewWBET"><div class="styles__ShareContainer-sc-1vaplep-1 cjQccr"><div class="styles__ShareSvgContainer-sc-1vaplep-3 hFhhob"><svg width="32" height="32"><use xlink:href="/chunks/icons/synopsis-icons-96c193d5.svg#icon-share">
-    
-    
-    </use></svg></div><div class="styles__ShareText-sc-1vaplep-2 jOJFqj">Share</div></div></section></div></div></div> */}
